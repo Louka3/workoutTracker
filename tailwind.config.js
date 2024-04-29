904bdcec83d7f7b77feb71c57401c08dfa723bcf
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-04-29 00:07:12 -0400

    added color palette and font styles/imports to the tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,37 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: "#000000",
+        background: "#eff0f0",
+        primary: "#58e9c7",
+        secondary: "#66998d",
+        accent: "#26e3b7",
+        "text-dark": "#ffffff",
+        "background-dark": "#0f1010",
+        "primary-dark": "#16a785",
+        "secondary-dark": "#66998d",
+        "accent-dark": "#1cd9ad",
+      },
+      fontSize: {
+        sm: "0.750rem",
+        base: "1rem",
+        xl: "1.333rem",
+        "2xl": "1.777rem",
+        "3xl": "2.369rem",
+        "4xl": "3.158rem",
+        "5xl": "4.210rem",
+      },
+      fontFamily: {
+        heading: "Montserrat",
+        body: "Montserrat",
+      },
+      fontWeight: {
+        normal: "400",
+        bold: "700",
+      },
+    },
   },
   plugins: [],
 };
