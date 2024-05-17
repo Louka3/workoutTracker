bfe7e83b00c8aaa69e1261134b14d286bbc1ac28
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-05-16 23:43:06 -0400

    finished layout for workout page. Moving onto server

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { WorkoutListComponent } from './workout-components/workout-list/workout-list.component';
+import { WorkoutPageComponent } from './workout-components/workout-page/workout-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'workout-list', component: WorkoutListComponent },
+  { path: 'workout-list/:workoutName', component: WorkoutPageComponent },
 ];
