e0132617b70c65afd98829dcb5b5edb338c4f987
Louis Kuczykowski <louisk@Louiss-Mac-mini.local>
2024-06-11 21:26:28 -0400

    Deleted unnecessary components. I already moved the components to a module and no longer need the originals.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { WorkoutListComponent } from './workout-components/workout-list/workout-list.component';
-import { WorkoutPageComponent } from './workout-components/workout-page/workout-page.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -19,6 +17,4 @@ export const routes: Routes = [
         (m) => m.WorkoutPagesModule
       ),
   },
-  // { path: 'workout-list', component: WorkoutListComponent },
-  // { path: 'workout-list/:workoutName', component: WorkoutPageComponent },
 ];
